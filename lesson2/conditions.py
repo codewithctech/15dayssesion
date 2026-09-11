39c2cedf3074a62baf874051fe41d34d2ad356fd
SierraLoneDeveloper <203786961+SierraLoneDeveloper@users.noreply.github.com>
2026-09-11 20:13:08 +0000

    day3

---
+++ lesson2/conditions.py
@@ -0,0 +1,6 @@
+age = 10
+
+if (age == 10):
+    print("Age is 10")
+else:
+    print("Age is not 10")
---
+++ lesson3/loops.py
@@ -0,0 +1,11 @@
+# Loops means repeating a action, Just Keeps the action ongoing
+#Advantages
+# - Allow us to perform task sequencially
+# - Used for multiple dataset
+
+#Disadvantages
+# - Memory Comsuming
+# - Might Crash middle a program if an error occurs
+
+for i in range(5):
+    print(i)
